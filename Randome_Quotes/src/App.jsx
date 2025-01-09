ecf4ffce01f0d7828a4e6655d672792412f0e119
dhaval <dgohel399@gmail.com>
2025-01-09 16:41:31 +0530

    changes

--- Randome_Quotes/src/App.jsx
+++ Randome_Quotes/src/App.jsx
@@ -3,19 +3,20 @@ import Button from './component/Button'
 import Quote from './component/Quote'
 
 function App() {
-  const [quote, setQuote] = useState('')
-  const [author, setAuthor] = useState('')
+  const [quoteData, setQuoteData] = useState({ quote: '', author: '' });
+  const [loading, setLoading] = useState(false);
 
   const fetchQuote = async () => {
+    setLoading(true); 
     try {
-      const response = await fetch('https://quotes-api-self.vercel.app/quote'); // Fetch API data
-      const data = await response.json(); // Convert to JSON
-
-      // Update state with quote and author
-      setQuote(data.quote);
-      setAuthor(data.author);
+      const response = await fetch('https://quotes-api-self.vercel.app/quote');
+      const data = await response.json();
+      
+      setQuoteData({ quote: data.quote, author: data.author });
     } catch (error) {
       console.error('Error fetching quote:', error);
+    } finally {
+      setLoading(false); 
     }
   };
 
@@ -25,7 +26,11 @@ function App() {
 
   return (
     <div className='flex flex-col justify-center items-center mt-10'>
-      <Quote quote={quote} author={author} />
+      {loading ? (
+        <p className="text-lg text-gray-600">Loading...</p>
+      ) : (
+        <Quote quote={quoteData.quote} author={quoteData.author} />
+      )}
       <Button fetchQuote={fetchQuote} />
     </div>
   )
--- Randome_Quotes/src/component/Quote.jsx
+++ Randome_Quotes/src/component/Quote.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Quote({quote, author}) {
   return (
-    <div className="text-center p-4 border rounded shadow-lg mt-10">
-        <p className="text-xl font-bold mb-2">"{quote}"</p>
-        <p className="text-gray-600">- {author}</p>
+    <div className="text-center bg-black p-4 rounded-xl shadow-lg mt-10">
+        <p className="text-xl font-bold mb-2 text-white">"{quote}"</p>
+        <p className="text-gray-400">- {author}</p>
     </div>
   )
 }
